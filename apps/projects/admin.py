1a685743eee2d666154aa799e2dfa61583d2aa6b
Reiley Angelo Muncal <reileyangelomuncal@gmail.com>
2026-04-25 21:34:53 +0800

    duplicated seal, fixed projects not appearing for engineering

--- apps/projects/admin.py
+++ apps/projects/admin.py
@@ -1,7 +1,4 @@
 from django.contrib import admin
-from .models import InfrastructureProject
-
 
 # InfrastructureProject is not registered in admin
-# Admins cannot manage projects through Django admin
 # Only engineering office users can create/edit/delete projects through the web interface
