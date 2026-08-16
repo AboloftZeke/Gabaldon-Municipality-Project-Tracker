5839470228e9373b929d796756c811952929908b
sanjuanjojo00 <sanjuanjojo00@gmail.com>
2026-08-16 18:45:20 +0800

    Allowed mayors office to make none infra projects

--- apps/non_infrastructure/forms.py
+++ apps/non_infrastructure/forms.py
@@ -1,80 +1,153 @@
 from django import forms
 
 from apps.system.models import (
-    Project,
-    Non_Infrastructure_Project,
     Address,
     NonInfrastructureCategory,
+    Non_Infrastructure_Project,
+    Project,
+    Project_Image,
 )
 
 
 class NonInfrastructureProjectForm(forms.Form):
-    title = forms.CharField(required=True, max_length=255)
-    description = forms.CharField(required=False, widget=forms.Textarea, max_length=2000)
-    location = forms.CharField(required=False, max_length=100)
-    implementing_office = forms.CharField(required=False, max_length=255)
-    category = forms.ModelChoiceField(
-            queryset=NonInfrastructureCategory.objects.all().order_by('type_name'),
-            required=False,
-            empty_label='Select Category'
-        )
-    service_description = forms.CharField(required=False, widget=forms.Textarea)
-    beneficiaries_description = forms.CharField(required=False, widget=forms.Textarea)
-    service_location_details = forms.CharField(required=False, max_length=255)
-    service_period = forms.CharField(required=False, max_length=100)
-    service_time = forms.TimeField(required=False)
-    budget_cost = forms.DecimalField(required=False, max_digits=15, decimal_places=2)
-    results_achieved = forms.CharField(required=False, widget=forms.Textarea)
-    source_of_fund = forms.CharField(required=False, max_length=255)
-    planned_start_date = forms.DateField(required=False)
-    planned_end_date = forms.DateField(required=False)
-    actual_start_date = forms.DateField(required=False)
-    revised_completion_date = forms.DateField(required=False)
+    non_infra_name = forms.CharField(required=True, max_length=255)
+    description = forms.CharField(required=False, widget=forms.Textarea(attrs={'rows': 4}), max_length=2000)
+    non_infra_category = forms.ModelChoiceField(
+        queryset=NonInfrastructureCategory.objects.all().order_by('type_name'),
+        required=False,
+        empty_label='Select Category',
+    )
+    event_date = forms.DateField(required=False, widget=forms.DateInput(attrs={'type': 'date'}))
+    start_time = forms.TimeField(required=False, widget=forms.TimeInput(attrs={'type': 'time'}))
+    end_time = forms.TimeField(required=False, widget=forms.TimeInput(attrs={'type': 'time'}))
+    venue_name = forms.CharField(required=False, max_length=255)
+    street = forms.CharField(required=False, max_length=500)
+    barangay = forms.CharField(required=False, max_length=200)
+    municipality = forms.CharField(required=False, max_length=200)
+    province = forms.CharField(required=False, max_length=200)
     latitude = forms.DecimalField(required=False, max_digits=10, decimal_places=7)
     longitude = forms.DecimalField(required=False, max_digits=10, decimal_places=7)
-    overall_progress_percentage = forms.DecimalField(required=False, max_digits=5, decimal_places=2)
+    project_images = forms.CharField(
+        required=False,
+        widget=forms.Textarea(attrs={
+            'rows': 4,
+            'placeholder': 'Paste one image URL per line\nhttps://example.com/image-1.jpg\nhttps://example.com/image-2.jpg',
+        }),
+    )
 
     def __init__(self, *args, **kwargs):
         self.instance = kwargs.pop('instance', None)
         super().__init__(*args, **kwargs)
 
+        if self.instance is not None:
+            normalized = self._resolve_instance(self.instance)
+            if normalized is not None:
+                self.fields['non_infra_name'].initial = normalized.non_infra_name
+                self.fields['description'].initial = normalized.description
+                self.fields['non_infra_category'].initial = normalized.non_infra_category_id
+                self.fields['event_date'].initial = normalized.event_date
+                self.fields['start_time'].initial = normalized.start_time
+                self.fields['end_time'].initial = normalized.end_time
+                self.fields['venue_name'].initial = normalized.venue_name
+                if normalized.address:
+                    self.fields['street'].initial = normalized.address.street
+                    self.fields['barangay'].initial = normalized.address.barangay
+                    self.fields['municipality'].initial = normalized.address.municipality
+                    self.fields['province'].initial = normalized.address.province
+                    self.fields['latitude'].initial = normalized.address.latitude
+                    self.fields['longitude'].initial = normalized.address.longitude
+                image_urls = [img.image_url for img in normalized.project.images.all() if img.image_url]
+                if image_urls:
+                    self.fields['project_images'].initial = '\n'.join(image_urls)
+
+    @staticmethod
+    def _resolve_instance(instance):
+        if isinstance(instance, Non_Infrastructure_Project):
+            return instance
+        non_infra_id = getattr(instance, 'non_infra_id', None)
+        if non_infra_id is None:
+            return None
+        return (
+            Non_Infrastructure_Project.objects
+            .select_related('project', 'address', 'non_infra_category')
+            .filter(non_infra_id=non_infra_id)
+            .first()
+        )
+
+    def _save_images(self, project):
+        raw_images = self.cleaned_data.get('project_images') or ''
+        urls = []
+        for line in raw_images.replace(',', '\n').splitlines():
+            value = line.strip()
+            if value:
+                urls.append(value)
+
+        project.images.all().delete()
+        for url in urls:
+            Project_Image.objects.create(project=project, image_url=url)
+
     def save(self, user=None, instance=None):
         data = self.cleaned_data
 
-        if instance is None:
-            proj = Project.objects.create(project_type='non_infrastructure', created_by=user, updated_by=user)
+        if instance is not None:
+            normalized_instance = self._resolve_instance(instance)
+            if normalized_instance is not None:
+                project = normalized_instance.project
+                non = normalized_instance
+            else:
+                project = Project.objects.create(
+                    project_type='non_infrastructure',
+                    created_by_user=user,
+                    updated_by_user=user,
+                )
+                non = Non_Infrastructure_Project.objects.create(project=project)
         else:
-            proj = Project.objects.filter(project_id=instance.non_infra_id).first()
-            if proj is None:
-                proj = Project.objects.create(project_type='non_infrastructure', created_by=user, updated_by=user)
+            project = Project.objects.create(
+                project_type='non_infrastructure',
+                created_by_user=user,
+                updated_by_user=user,
+            )
+            non = Non_Infrastructure_Project.objects.filter(project=project).first()
+            if non is None:
+                non = Non_Infrastructure_Project(project=project)
 
-        non = Non_Infrastructure_Project.objects.filter(project=proj).first()
-        if non is None:
-            non = Non_Infrastructure_Project(project=proj)
-
-        non.non_infra_name = data.get('title')
+        non.non_infra_name = data.get('non_infra_name') or non.non_infra_name
         non.description = data.get('description') or ''
-        non.non_infra_category = data.get('category')
+        non.non_infra_category = data.get('non_infra_category')
+        non.event_date = data.get('event_date')
+        non.start_time = data.get('start_time')
+        non.end_time = data.get('end_time')
+        non.venue_name = data.get('venue_name') or ''
 
-        loc = data.get('location')
-        if loc:
+        street = data.get('street') or ''
+        barangay = data.get('barangay') or ''
+        municipality = data.get('municipality') or 'Gabaldon'
+        province = data.get('province') or 'Nueva Ecija'
+        latitude = data.get('latitude')
+        longitude = data.get('longitude')
+
+        if street or barangay or municipality or province or latitude is not None or longitude is not None:
             if non.address:
-                non.address.address = loc
-                non.address.municipality = "Gabaldon"
-                non.address.province = "Nueva Ecija"
-                non.address.save()
+                addr = non.address
             else:
                 addr = Address.objects.create(
-                    barangay=loc,
-                    municipality="Gabaldon",
-                    province="Nueva Ecija"
+                    street=street,
+                    barangay=barangay,
+                    municipality=municipality,
+                    province=province,
                 )
                 non.address = addr
 
-        non.event_date = data.get('planned_start_date')
-        non.start_time = data.get('service_time')
-        non.end_time = None
-        non.save()
+            addr.street = street or addr.street
+            addr.barangay = barangay or addr.barangay
+            addr.municipality = municipality or addr.municipality
+            addr.province = province or addr.province
+            addr.latitude = latitude if latitude is not None else addr.latitude
+            addr.longitude = longitude if longitude is not None else addr.longitude
+            addr.save()
+            non.address = addr
 
+        non.save()
+        self._save_images(project)
         return non
 
--- apps/non_infrastructure/tests.py
+++ apps/non_infrastructure/tests.py
@@ -1,10 +1,98 @@
+from django.contrib.auth import get_user_model
 from django.test import TestCase
 
 from apps.non_infrastructure.forms import NonInfrastructureProjectForm
+from apps.system.models import (
+    NonInfrastructureCategory,
+    Non_Infrastructure_Project,
+    Project,
+    Project_Image,
+)
 
 
 class NonInfrastructureProjectFormTests(TestCase):
-    def test_form_accepts_instance_keyword(self):
-        form = NonInfrastructureProjectForm(instance=object())
-        self.assertIsNotNone(form)
-        self.assertIsNotNone(form.instance)
+    def setUp(self):
+        self.user = get_user_model().objects.create_user(
+            username='project-admin',
+            email='admin@example.com',
+            password='testpass123',
+        )
+        self.category = NonInfrastructureCategory.objects.create(
+            type_code='health-care',
+            type_name='Health Care',
+            description='Health care services',
+        )
+
+    def test_form_saves_normalized_non_infrastructure_project(self):
+        form = NonInfrastructureProjectForm(
+            data={
+                'non_infra_name': 'Community Health Fair',
+                'description': 'Free medical and health education event.',
+                'non_infra_category': str(self.category.non_infrastructure_category_id),
+                'event_date': '2026-08-20',
+                'start_time': '08:00',
+                'end_time': '12:00',
+                'venue_name': 'Municipal Plaza',
+                'street': 'Rizal Street',
+                'barangay': 'Mabini',
+                'municipality': 'Gabaldon',
+                'province': 'Nueva Ecija',
+                'latitude': '15.3000000',
+                'longitude': '121.3500000',
+                'project_images': 'https://example.com/one.jpg\nhttps://example.com/two.jpg',
+            }
+        )
+
+        self.assertTrue(form.is_valid(), form.errors)
+        project = form.save(user=self.user)
+
+        self.assertEqual(project.non_infra_name, 'Community Health Fair')
+        self.assertEqual(project.non_infra_category, self.category)
+        self.assertEqual(project.address.barangay, 'Mabini')
+        self.assertEqual(project.venue_name, 'Municipal Plaza')
+        self.assertEqual(project.project.created_by_user, self.user)
+        self.assertEqual(
+            list(project.project.images.order_by('project_image_id').values_list('image_url', flat=True)),
+            ['https://example.com/one.jpg', 'https://example.com/two.jpg'],
+        )
+
+    def test_form_updates_existing_project_and_images(self):
+        proj = Project.objects.create(project_type='non_infrastructure', created_by_user=self.user, updated_by_user=self.user)
+        existing = Non_Infrastructure_Project.objects.create(
+            project=proj,
+            non_infra_name='Old Program',
+            description='Old description',
+            non_infra_category=self.category,
+            event_date='2026-01-10',
+            venue_name='Old Venue',
+        )
+        Project_Image.objects.create(project=proj, image_url='https://example.com/old.jpg')
+
+        form = NonInfrastructureProjectForm(
+            instance=existing,
+            data={
+                'non_infra_name': 'Updated Community Fair',
+                'description': 'Updated description.',
+                'non_infra_category': str(self.category.non_infrastructure_category_id),
+                'event_date': '2026-09-15',
+                'start_time': '09:00',
+                'end_time': '13:00',
+                'venue_name': 'New Event Grounds',
+                'street': 'Main Avenue',
+                'barangay': 'San Francisco',
+                'municipality': 'Gabaldon',
+                'province': 'Nueva Ecija',
+                'project_images': 'https://example.com/new-one.jpg\nhttps://example.com/new-two.jpg',
+            },
+        )
+
+        self.assertTrue(form.is_valid(), form.errors)
+        updated = form.save(user=self.user, instance=existing)
+
+        self.assertEqual(updated.non_infra_name, 'Updated Community Fair')
+        self.assertEqual(updated.event_date.isoformat(), '2026-09-15')
+        self.assertEqual(updated.venue_name, 'New Event Grounds')
+        self.assertEqual(
+            list(updated.project.images.order_by('project_image_id').values_list('image_url', flat=True)),
+            ['https://example.com/new-one.jpg', 'https://example.com/new-two.jpg'],
+        )
