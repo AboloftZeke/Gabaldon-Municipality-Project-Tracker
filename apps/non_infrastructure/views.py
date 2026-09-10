f5264b9880ec61880c434dd40b7abc9d0816f4f9
KURBY DELOS SANTOS <kurbydelossantos503@gmail.com>
2026-09-11 00:45:09 +0800

    Describe non-infrastructure checker submission workflow

--- apps/non_infrastructure/views.py
+++ apps/non_infrastructure/views.py
@@ -317,7 +317,7 @@ class NonInfrastructureProjectSubmitForReviewView(
             messages.success(
                 request,
                 f'Revision {revision.revision_number} was submitted for '
-                'administrator review.',
+                'Non-Infrastructure Project Checker review.',
             )
         return redirect(
             'mayor_projects:non_infrastructure_project_detail',
